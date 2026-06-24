6bc975e3b601b6460b78ae527edf200b742daeed
Isaac Liang <burstfire23@gmail.com>
2026-06-23 17:54:50 -0700

    added tests for divide(), added more tests for previous functions, and fixed the issue of calling the wrong function in the previous tests

--- script.spec.js
+++ script.spec.js
@@ -2,43 +2,85 @@ import {add, subtract, multiply, divide} from "./script.js";
 
 
 describe("add", () => {
-    test("positive numbers", () =>{
+    test("positive integers", () =>{
         expect(add(2, 5)).toBe(5);
     });
 
-    test("negative numbers", () =>{
+    test("negative integers", () =>{
         expect(add(-4, -6)).toBe(-10);
     });
 
-    test("mixed numbers", () =>{
+    test("mixed integers", () =>{
         expect(add(-2, 7)).toBe(5);
+    });
+
+    test("mixed numbers", () =>{
+        expect(add(-5.2, 7.1)).toBe(1.9);
     });
 });
 
 describe("subtract", () => {
-    test("positive numbers", () =>{
-        expect(add(8, 2)).toBe(6);
+    test("positive integers", () =>{
+        expect(subtract(8, 2)).toBe(6);
     });
 
-    test("negative numbers", () =>{
-        expect(add(-7, -1)).toBe(-6);
+    test("negative integers", () =>{
+        expect(subtract(-7, -1)).toBe(-6);
+    });
+
+    test("mixed integers", () =>{
+        expect(subtract(-5, 2)).toBe(-7);
     });
 
     test("mixed numbers", () =>{
-        expect(add(-5, 2)).toBe(-7);
+        expect(subtract(5.2, -7.1)).toBe(12.3);
     });
 });
 
 describe("multiply", () => {
-    test("positive numbers", () =>{
-        expect(add(2, 4)).toBe(8);
+    test("positive integers", () =>{
+        expect(multiply(2, 4)).toBe(8);
     });
 
-    test("negative numbers", () =>{
-        expect(add(-5, -3)).toBe(15);
+    test("negative integers", () =>{
+        expect(multiply(-5, -3)).toBe(15);
+    });
+
+    test("mixed integers", () =>{
+        expect(multiply(-3, 4)).toBe(-12);
     });
 
     test("mixed numbers", () =>{
-        expect(add(-3, 4)).toBe(-12);
+        expect(multiply(8.1, -3.9)).toBe(-31.59);
+    });
+});
+
+describe("divide", () => {
+    test("positive integers", () =>{
+        expect(divide(4, 2)).toBe(2);
+    });
+
+    test("negative integers", () =>{
+        expect(divide(-10, -5)).toBe(2);
+    });
+
+    test("mixed integers", () =>{
+        expect(divide(-15, 3)).toBe(-5);
+    });
+
+    test("mixed numbers", () =>{
+        expect(divide(8, -5)).toBe(-1.6);
+    });
+
+    test("positive divide by zero", () =>{
+        expect(divide(8, 0)).toBe(Infinity);
+    });
+
+    test("negative divide by zero", () =>{
+        expect(divide(8, 0)).toBe(-Infinity);
+    });
+
+    test("zero divide by zero", () =>{
+        expect(divide(0, 0)).toBe(NaN);
     });
 });

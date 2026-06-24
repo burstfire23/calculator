d73d9fda01cdaadde8a9c2d4520002f68d8b9133
Isaac Liang <burstfire23@gmail.com>
2026-06-23 17:21:41 -0700

    more testing setup to be able to use import/export and created the shell main calculator functions (add, subtract, multiply, divide) for testing purposes

---
+++ script.js
@@ -0,0 +1,15 @@
+export function add(num1, num2) {
+    return 0;
+}
+
+export function subtract(num1, num2) {
+    return 0;
+}
+
+export function multiply(num1, num2) {
+    return 0;
+}
+
+export function divide(num1, num2) {
+    return 0;
+}

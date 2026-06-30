c2f6d96cd4330685f1c30f22d7b6ae2073ae1915
Isaac Liang <burstfire23@gmail.com>
2026-06-29 22:00:17 -0700

    added backspace button

--- ui.js
+++ ui.js
@@ -1,6 +1,6 @@
 import {updateOperand, updateOperator, setNum1, setNum2, setOperator, setResult, calculate, getNum1, getNum2, getOperator, getResult} from "./calculator.js"
 
-
+const root = document.documentElement;
 const display = document.querySelector(".display");
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
@@ -8,6 +8,7 @@ const misc = document.querySelectorAll(".misc");
 const clearButton = document.querySelector(".clear");
 const equalsButton = document.querySelector(".equals");
 const toggleButton = document.querySelector(".toggle");
+const backspace = document.querySelector(".backspace");
 
 function displayItem(item){
     const p = document.createElement("p");
@@ -65,7 +66,10 @@ function handleOperatorsClick(text) {
 function handleEqualsClick(){
     let answer = calculate();
     clear();
-    displayItem(answer);
+
+    for (let letter of answer) {
+        displayItem(letter);
+    }
     setNum1(answer);
     setResult(answer);
 
@@ -106,7 +110,8 @@ function handleNegativeClick() {
 function toggleTheme() {
     const root = document.documentElement; 
   	const newTheme = root.className === "dark" ? "light" : "dark"; 
-  	const toggleIcon = document.querySelector("img");
+  	const toggleIcon = document.querySelector(".toggle img");
+    const backspaceIcon = document.querySelector(".backspace img");
     root.className = newTheme;
 
     if (newTheme == "dark") {
@@ -114,11 +119,35 @@ function toggleTheme() {
         toggleIcon.alt = "sun icon";
         toggleIcon.width = "40";
 
+        backspaceIcon.src = "./images/backspace-white.png";
+        backspaceIcon.alt = "white backpace icon";
+        backspaceIcon.width = "40";
+
     } else {
         toggleIcon.src = "./images/moon.png";
         toggleIcon.alt = "moon icon";
         toggleIcon.width = "40";
 
+        backspaceIcon.src = "./images/backspace-black.png";
+        backspaceIcon.alt = "black backpace icon";
+        backspaceIcon.width = "40";
+
+    }
+}
+
+function handleBackspace() {
+    if (getNum2() != "") {
+        let newNum2 = getNum2().split("").slice(0, -1).join("");
+        setNum2(newNum2);
+        display.removeChild(display.lastElementChild);
+    } else if (getOperator() != undefined) {
+        setOperator(undefined);
+        display.removeChild(display.lastElementChild);
+    } else if (getNum1() != "") {
+        let newNum1 = getNum1().split("").slice(0, -1).join("");
+        console.log(newNum1);
+        setNum1(newNum1);
+        display.removeChild(display.lastElementChild);
     }
 }
 
@@ -185,7 +214,11 @@ toggleButton.addEventListener("click", () => {
     event.currentTarget.blur();
 });
 
-const root = document.documentElement;
+backspace.addEventListener("click", () => {
+    handleBackspace();
+});
+
+
 root.addEventListener("keydown", (event) => {
     const key = event.key;
     const nums = "0123456789";
